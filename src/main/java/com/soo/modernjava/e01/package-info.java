37f93e857473d407818bb64e1607f09ea0479fb8
soo <runthe@naver.com>
2016-08-22 21:52:41 +0900

    패키지 초기셋팅

---
+++ src/main/java/com/soo/modernjava/e01/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by soo on 2016. 8. 22..
+ */
+package com.soo.modernjava.e01;
---
+++ src/main/java/com/soo/modernjava/e08_03_stream_03/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by soo on 2016. 8. 22..
+ */
+package com.soo.modernjava.e08_03_stream_03;
